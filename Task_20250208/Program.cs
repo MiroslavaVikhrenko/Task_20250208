04d1db960f87c7111a6b1d755fc809e1375e38b0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-09 16:44:25 -0700

    Add project files.

---
+++ Task_20250208/Program.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using Microsoft.VisualBasic;
+using System.Reflection;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+
+namespace Task_20250208
+{
+    internal class Program
+    {
+        static string connectionString = "Data Source=MIRUAHUA;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        static string connectionString2 = "Data Source=MIRUAHUA;Initial Catalog=Cars;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        static void Main(string[] args)
+        {
+            CreateDatabase();
+            CreateTable();
+
+            List<Car> cars = new List<Car>()
+            {
+                new Car(1, "Toyota1", 2016),
+                new Car(2, "Honda1", 2019),
+                new Car(3, "Mitsubishi1", 2017),
+                new Car(4, "Subaru1", 2015),
+                new Car(5, "Toyota2", 2020),
+                new Car(6, "Toyota3", 2019),
+                new Car(7, "Honda2", 2016),
+                new Car(8, "Mitsubishi2", 2022),
+                new Car(9, "Mitsubishi2", 2016),
+                new Car(10, "Toyota4", 2016)
+            };
+
+            PopulateTable(cars);
+            List<Car> cars2 = GetCarsByYear(2018);
+            Console.ReadKey();
+        }
+        static void CreateDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("CREATE DATABASE Cars", connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Database created");
+            }
+        }
+        static void CreateTable()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString2))
+            {
+                connection.Open();
+                string commandtext = $"""
+                    CREATE TABLE [Car]
+                    (
+                        Id int PRIMARY KEY,
+                        Model varchar(60) NOT NULL,
+                        Year int NOT NULL
+                    )
+                    """;
+                SqlCommand command = new SqlCommand(commandtext, connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table created");
+            }
+        }
+
+        static void PopulateTable(List<Car> cars)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString2))
+            {
+                connection.Open();
+
+                foreach (var car in cars)
+                {
+                    string commandtext = $"""
+                    INSERT INTO [Car] (Id, Model, Year)
+                    VALUES ({car.Id}, '{car.Model}', {car.Year} )
+                    """;
+                    SqlCommand command = new SqlCommand(commandtext, connection);
+                    command.ExecuteNonQuery();
+                }
+                
+                Console.WriteLine("Table populated");
+            }
+        }
+
+        static List<Car> GetCarsByYear(int search)
+        {
+            List <Car> cars = new List <Car>();
+            using (SqlConnection connection = new SqlConnection(connectionString2))
+            {
+                connection.Open();
+
+                string commandtext = $"""
+                    SELECT Id, Model, Year FROM [Car]
+                    WHERE Year > {search}
+                    """;
+                SqlCommand command = new SqlCommand(commandtext, connection);
+                //command.ExecuteNonQuery();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        string columnName1 = reader.GetName(0);
+                        string columnName2 = reader.GetName(1);
+                        string columnName3 = reader.GetName(2);
+                        Console.WriteLine($"{columnName1} | {columnName2} | {columnName3}");
+
+                        while (reader.Read())
+                        {
+                            object id = reader.GetValue(0);
+                            object model = reader.GetValue(1);
+                            object year = reader.GetValue(2);
+                            Console.WriteLine($"{id} | {model} | {year}");
+                            cars.Add(new Car(Convert.ToInt32(id), model.ToString(), Convert.ToInt32(year)));
+                        }
+                    }
+                }
+
+                Console.WriteLine("Table populated");
+            }
+
+            return cars;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car(int id, string model, int year)
+        {
+            Id = id; Model = model; Year = year;
+        }
+    }
+}
---
+++ Task_20250208_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Task_20250208_1
+/*
+         Создайте базу данных «Shop». В ней создайте таблицу «Phones» (опишите на основе класса): 
+        Id (Identity), Manufacturer, Model, Year, Price. 
+
+Определите следующие методы для выполнения запросов:
+
+1) Метод заполнения данными. Добавить минимум 3 телефона (заполнение базы данных выполнить через List объектов класса). 
+2) Метод считывания данных. Добавить в новую коллекцию все содержимое таблицы, используя типизацию результатов, после вывести на экран.
+3) Метод удаления по идентификатору. Выполнить удаление рядка из таблицы по его первичному ключу.
+4) Метод редактирования по идентификатору. Выполнить редактирования всех столбцов таблицы определенного рядка по его первичному ключу.
+         */
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            IConfiguration configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened");
+            }
+
+            CreateDatabase(connectionString);
+
+            string connectionString2 = configuration.GetConnectionString("Shop");
+
+            Console.ReadKey();
+            
+        }
+
+        static void CreateTable(string connectionString)
+        {
+            //В ней создайте таблицу «Phones» (опишите на основе класса): 
+            //Id(Identity), Manufacturer, Model, Year, Price.
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string commandtext = $"""
+                    CREATE TABLE [Phones]
+                    (
+                        Id int PRIMARY KEY,
+                        Manufacturer nvarchar(60) NOT NULL,
+                        Model nvarchar(60) NOT NULL,
+                        Year int NOT NULL,
+                        Price int NOT NULL
+                    )
+                    """;
+                SqlCommand command = new SqlCommand(commandtext, connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table created");
+            }
+        }
+
+        static void CreateDatabase(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("CREATE DATABASE Shop", connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("Database created");
+            }
+        }
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+    }
+}
