f96bbe5e33ed0e283ba93f5debe251d44852a284
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-09 17:03:09 -0700

    Create db, create table, populate table

--- Task_20250208_1/Program.cs
+++ Task_20250208_1/Program.cs
@@ -35,8 +35,46 @@ namespace Task_20250208_1
 
             string connectionString2 = configuration.GetConnectionString("Shop");
 
+            CreateTable(connectionString2);
+
+            List<Phone> phones = new List<Phone>()
+            {
+                new Phone(1, "iPhone", "16 Pro Max", 2024, 1200),
+                new Phone(2, "iPhone", "15 Pro Max", 2023, 1000),
+                new Phone(3, "iPhone", "15 Plus", 2023, 1100),
+                new Phone(4, "iPhone", "14 Pro Max", 2022, 900),
+                new Phone(5, "iPhone", "SE", 2022, 1000),
+                new Phone(6, "iPhone", "13 Pro Mini", 2021, 1000),
+                new Phone(7, "Samsung", "Galaxy S24", 2024, 1100),
+                new Phone(8, "Samsung", "Galaxy A05", 2023, 1050),
+                new Phone(9, "Samsung", "Galaxy M35 5G", 2024, 1150),
+                new Phone(10, "Samsung", "A55 5G", 2024, 1100)
+            };
+
+            PopulateTable(connectionString2, phones);
+
             Console.ReadKey();
             
+        }
+
+        static void PopulateTable(string connectionString, List<Phone> phones)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (var phone in phones)
+                {
+                    string commandtext = $"""
+                    INSERT INTO [Phones] (Id, Manufacturer, Model, Year, Price)
+                    VALUES ({phone.Id},'{phone.Manufacturer}', '{phone.Model}', {phone.Year}, {phone.Price})
+                    """;
+                    SqlCommand command = new SqlCommand(commandtext, connection);
+                    command.ExecuteNonQuery();
+                }
+
+                Console.WriteLine("Table populated");
+            }
         }
 
         static void CreateTable(string connectionString)
@@ -82,5 +120,10 @@ namespace Task_20250208_1
         public string Model { get; set; }
         public int Year { get; set; }
         public int Price { get; set; }
+
+        public Phone(int id, string manufacturer, string model, int year, int price)
+        {
+            Id = id; Manufacturer = manufacturer; Model = model; Year = year; Price = price;
+        }
     }
 }
