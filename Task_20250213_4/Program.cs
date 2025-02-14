b656274bc5722dd3ff243d722058ebf2ea87a4d2
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-14 00:37:10 -0700

    Practice ExecuteScalar()

---
+++ Task_20250213_4/Program.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Task_20250213_4
+{
+    internal class Program
+    {
+        /*
+          Создать класс «Студент», с полями: Id, Fio, Age, AverageMark. 
+        Создать таблицу в базе данных, заполнить. 
+        Используя получение скалярных значений, выполнить следующие действия:
+
+Получить средний балл всех учеников.
+Получить количество учеников.
+Получить минимальную и максимальную оценку среди учеников.
+Получить сумму оценок всего класса.
+Считать всех учеников в коллекцию и вывести на экран.
+         */
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            IConfiguration configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened");
+
+                SqlCommand command = new SqlCommand("SELECT AVG(AverageMark) FROM Student", connection);
+                object avg = command.ExecuteScalar();
+                Console.WriteLine($"Avg grade is {avg}");
+
+                command.CommandText = "SELECT COUNT(Id) FROM Student";
+                object count = command.ExecuteScalar();
+                Console.WriteLine($"Total: {count} students");
+
+                command.CommandText = "SELECT MIN(AverageMark) FROM Student";
+                object min = command.ExecuteScalar();
+                Console.WriteLine($"Min grade is {min}");
+
+                command.CommandText = "SELECT MAX(AverageMark) FROM Student";
+                object max = command.ExecuteScalar();
+                Console.WriteLine($"Max grade is {max}");
+
+                command.CommandText = "SELECT SUM(AverageMark) FROM Student";
+                object sum = command.ExecuteScalar();
+                Console.WriteLine($"Sum grade is {sum}");
+
+                List<Student> students = GetStudents(connectionString);
+
+                foreach (Student student in students)
+                {
+                    Console.WriteLine($"{student.Id} | {student.Fio} | {student.Age} | {student.AverageMark}");
+                }
+            }
+        }
+
+        static List<Student> GetStudents(string connectionString)
+        {
+            List<Student> students = new List<Student>();
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string commandText = $"""
+                    SELECT Id, Fio, Age, AverageMark
+                    FROM [Student]
+                    """;
+                SqlCommand command = new SqlCommand(commandText, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        string columnName1 = reader.GetName(0);
+                        string columnName2 = reader.GetName(1);
+                        string columnName3 = reader.GetName(2);
+                        string columnName4 = reader.GetName(3);
+                        Console.WriteLine($"> {columnName1} | {columnName2} | {columnName3} | {columnName4}");
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string fio = reader.GetString(1);
+                            int age = reader.GetInt32(2);
+                            int avgmark = reader.GetInt32(3);
+
+                            students.Add(new Student(id, fio, age, avgmark));  
+                        }
+                    }
+                }
+
+            }
+            return students;
+        }
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fio { get; set; }
+        public int Age { get; set; }
+        public int AverageMark { get; set; }
+
+        public Student(int id, string fio, int age, int avgmark)
+        {
+            Id = id;
+            Fio = fio;
+            Age = age;
+            AverageMark = avgmark;
+        }
+    }
+}
---
+++ Task_20250213_5/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Task_20250213_5
+{
+    internal class Program
+    {
+        /*
+         У вас есть база данных с таблицей «Employees», которая содержит информацию о сотрудниках, включая их идентификаторы, имена, возраст, должности и зарплаты. 
+
+1) Создайте метод, который позволяет пользователю ввести возраст, 
+        а затем отправляет скалярный запрос в базу данных для получения средней зарплаты 
+        сотрудников старше указанного возраста, используя параметризацию.
+
+2) Напишите запрос, который возвращает количество сотрудников для каждой должности. Используйте группировку по должностям и агрегатную функцию COUNT() для подсчета числа сотрудников на каждой должности.
+
+3) Напишите запрос, который для каждого возраста находит максимальную зарплату среди всех сотрудников этого возраста. Используйте группировку по возрасту и агрегатную функцию MAX() для нахождения максимальной зарплаты.
+
+4) Напишите запрос, который выполняет удаление сотрудника по его идентификатору. При выполнении запроса, используйте параметризацию.
+
+5) Напишите запрос, который выполняет обновление зарплаты для сотрудников, идентификаторы которых входят в список переданных. При выполнении запроса, используйте параметризацию.
+         */
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            IConfiguration configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened");
+            }
+
+        }
+
+        static void GetAvgSalaryByAge(string connectionString, int age)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    string sql = $"SELECT AVG(Salary) FROM Employees ";
+                }
+            }
+        }
+    }
+}
